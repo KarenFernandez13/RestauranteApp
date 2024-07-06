23d08f032d1af5347ce727d67382bc9930b337e3
Karen Fernandez <145879499+KarenFernandez13@users.noreply.github.com>
2024-07-05 22:16:57 -0300

    cambios en front

--- Restaurante/obj/Debug/net8.0/Restaurante.AssemblyInfo.cs
+++ Restaurante/obj/Debug/net8.0/Restaurante.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3951235501cfdaa2d93e40c61ff93be4aff200")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80c4448300627f7b827221b323ad69ebd11decb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
